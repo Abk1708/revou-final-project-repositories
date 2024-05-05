5b8287f6edbe6222ffcb6e22296e6dd70305b52e
alvidofaisal <alvidofaisal@gmail.com>
2024-05-06 00:00:52 +0700

    Fix: Enhance performance of news sea rch functionality

--- server/features/news/routes.py
+++ server/features/news/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 import requests
+from concurrent.futures import ThreadPoolExecutor
 import random
 
 # Create a Blueprint for the news feature
@@ -12,7 +13,7 @@ def call_api(endpoint):
         response = requests.get(url)
         response.raise_for_status()  
         return response.json()
-    except requests.exceptions.RequestException as e:
+    except requests.HTTPError as e:
         return {"error": str(e)}
 
 # Define a route for getting news
@@ -23,10 +24,8 @@ def get_news():
         "title": [
             "desa digital", "smart village", "teknologi pedesaan",
             "infrastruktur digital desa", "internet pedesaan", "akses internet desa",
-            "Kemkominfo desa digital", "Kominfo pedesaan",
-            "berita daerah", "berita lokal", "berita regional",  
-            "pemerintah daerah", "kebijakan daerah", "perkembangan daerah",
-            "teknologi", "digital", "internet", # Additional keywords
+            "Kemkominfo", "Kementerian Komunikasi dan Informasi",
+            "cloud", "digital", "infrastruktur", "internet", "penemuan", "perangkat", "perkembangan", "teknologi", # Additional keywords
         ],
     }
 
@@ -35,20 +34,44 @@ def get_news():
         endpoints = [
             'api/antara-news/tekno', 
             'api/tempo-news/tekno', 
+            'api/tribun-news/zones/techno',
             ]
+        
+        # List of all zones in Tribun News
+        tribun_news_zones = [
+            'Jabar', 'Mataram', 'Medan', 'Padang', 'Flores', 'Ambon', 'Bogor', 'Pantura',
+            'Palembang', 'Pekanbaru', 'Banjarmasin', 'Pontianak', 'Papua', 'Cirebon', 'Jogja',
+            'Bangka', 'Jambi', 'Palu', 'Papuabarat', 'Banten', 'Jateng', 'Aceh', 'Sumsel', 'Kalteng',
+            'Makassar', 'Solo', 'Belitung', 'Kaltara', 'Lombok', 'Banyumas', 'Sultra', 'Babel', 'Kupang',
+            'Ternate', 'Manado'
+            ]
+        
         # Randomize the order of the endpoints
         random.shuffle(endpoints)  
+        
+        # Use ThreadPoolExecutor to run API calls concurrently
+        tasks = []
+        with ThreadPoolExecutor() as executor:
+            for endpoint in endpoints:
+                # Create a list of endpoints or zones to iterate over
+                iterate_over = tribun_news_zones if 'zones' in endpoint else [None]
+                
+                for item in iterate_over:
+                    # Replace 'zones' with the actual zone if it exists
+                    api_endpoint = endpoint.replace('zones', item) if item else endpoint
+                    tasks.append(executor.submit(call_api, api_endpoint))
+            responses = [task.result() for task in tasks]
+                
+        # Process the responses        
         news_data = []
-        for endpoint in endpoints:
-            response_data = call_api(endpoint)
-            # If the response data contains a "data" key
+        for response_data in responses:        
             if 'data' in response_data:  
-                filtered_data = [newsItem for newsItem in response_data['data'] if any(keyword.lower() in newsItem['title'].lower() or (keyword.lower() in newsItem['description'].lower() if 'description' in newsItem else False) for keyword in params['title'])]
-                news_data.extend(filtered_data)
-
+                    filtered_data = [newsItem for newsItem in response_data['data'] if any(keyword.lower() in newsItem['title'].lower() or (keyword.lower() in newsItem['description'].lower() if 'description' in newsItem else False) for keyword in params['title'])]
+                    news_data.extend(filtered_data)
+                
         # Randomize the order of the news articles
         random.shuffle(news_data)  
-
         return jsonify(news_data)
-    except requests.exceptions.RequestException as e:
+    
+    except requests.HTTPError as e:
         return jsonify({"error": str(e)})
