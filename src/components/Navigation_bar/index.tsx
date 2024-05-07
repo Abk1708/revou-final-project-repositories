f81081505968bb75b73151bb9c335c06ff8d77a1
Blackpossum <yosaphatharwindra@gmail.com>
2024-05-08 00:00:40 +0700

    change route in component

--- src/components/Navigation_bar/index.tsx
+++ src/components/Navigation_bar/index.tsx
@@ -57,13 +57,13 @@ const Navigation = () => {
             </div>
             <div className="hidden md:flex items-center pr-4 space-x-3 rtl:space-x-reverse"> {/* tablet and larger */}
                 <button
-                    onClick={handleLogin}
+                    onClick={handleRegister}
                     type="button"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 > Get started
                 </button>
                 <button
-                    onClick={handleRegister}
+                    onClick={handleLogin}
                     type="button"
                     className="text-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:text-blue-700 dark:hover:text-white dark:focus:ring-blue-800 border border-blue-700 hover:bg-blue-700 hover:bg-opacity-10 dark:hover:bg-blue-700 dark:hover:bg-opacity-10"
                 > Already Have Account?
