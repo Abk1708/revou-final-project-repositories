c2fb45a134164c41f3d63a86dea0113b434a90cc
alvidofaisal <alvidofaisal@gmail.com>
2024-05-10 15:46:27 +0700

    Fix HTML render template on email verification

--- server/mail.py
+++ server/mail.py
@@ -5,7 +5,7 @@ mail = Mail()
 
 def send_verification_email(user_email, token):
     verification_link = url_for("auth.confirm_email", token=token, _external=True)
-    html_content = render_template('auth/templates/email_verification_template.html', verification_link=verification_link)
-    msg = Message('Confirm Your Email', sender='your-email@example.com', recipients=[user_email])
+    html_content = render_template('email_verification.html', verification_link=verification_link)
+    msg = Message('Confirm Your Email', sender='tech.for.village@outlook.com', recipients=[user_email])
     msg.html = html_content
     mail.send(msg)
